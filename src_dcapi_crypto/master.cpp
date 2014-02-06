746c4bdd96411eb6c7fab6f05d03e550f268e479
Nauchnik <zaikin.icc@gmail.com>
2014-02-06 19:54:55 +0900

    Modifications if creating crypto wus

--- src_dcapi_crypto/master.cpp
+++ src_dcapi_crypto/master.cpp
@@ -24,7 +24,6 @@
 
 using namespace std;
 
-const int PROBLEMS_IN_WU = 10000;
 const int MIN_WUS_FOR_CREATION = 200;
 
 // Number of results we have received so far
@@ -41,11 +40,13 @@ bool IsTasksFile;
 
 // Command line options
 
-struct cconfig_params_crypto {
+struct config_params_crypto {
+	string problem_type;
 	string settings_file;
 	string data_file;
 	int cnf_variables;
 	int cnf_clauses;
+	int problems_in_wu;
 	int unsent_needed_wus;
 	int total_wus;
 	int created_wus;
@@ -54,12 +55,12 @@ struct cconfig_params_crypto {
 static void print_help(const char *prog);
 bool do_work( vector<int> &wu_id_vec );
 void ParseConfigFile( string &cnf_name, string &cnf_head, stringstream &config_sstream );
-static void create_wus( latin_square &ls, stringstream &config_sstream, config_params &config_p, 
+static void create_wus( stringstream &config_sstream, config_params_crypto &config_p, 
 	                    string cnf_head, int wus_for_creation_count, vector<int> &wu_id_vec, bool &IsLastGenerating );
-bool find_sat( int cnf_index );
 void add_result_to_file( string output_filename, char *tag, char *id );
 void GetCountOfUnsentWUs( int &unsent_count );
 bool ProcessQuery( MYSQL *conn, string str, vector< vector<stringstream *> > &result_vec );
+bool find_sat( int cnf_index );
 
 static const struct option longopts[] =
 {
@@ -163,7 +164,7 @@ static void print_help(const char *prog)
 	exit(0);
 }
 
-void ParseConfigFile( config_params &config_p, string &cnf_head, stringstream &config_sstream )
+void ParseConfigFile( config_params_crypto &config_p, string &cnf_head, stringstream &config_sstream )
 {
 	fstream master_config_file;
 	string input_str, str1, str2, str3;
@@ -179,11 +180,23 @@ void ParseConfigFile( config_params &config_p, string &cnf_head, stringstream &c
 	}
 	cout << endl << "input file opened" << endl;
 	
-	// cnf_name
+	// problem_type
 	getline( master_config_file, input_str );
 	config_sstream << input_str << endl;
 	sstream << input_str;
-	sstream >> str1 >> str2 >> config_p.cnf_name;
+	sstream >> str1 >> str2 >> config_p.problem_type;
+	sstream.str(""); sstream.clear();
+	// settings_file
+	getline( master_config_file, input_str );
+	config_sstream << input_str << endl;
+	sstream << input_str;
+	sstream >> str1 >> str2 >> config_p.settings_file;
+	sstream.str(""); sstream.clear();
+	// data_file
+	getline( master_config_file, input_str );
+	config_sstream << input_str << endl;
+	sstream << input_str;
+	sstream >> str1 >> str2 >> config_p.data_file;
 	sstream.str(""); sstream.clear();
 	// cnf_variables
 	getline( master_config_file, input_str );
@@ -197,23 +210,11 @@ void ParseConfigFile( config_params &config_p, string &cnf_head, stringstream &c
 	sstream << input_str;
 	sstream >> str1 >> str2 >> config_p.cnf_clauses;
 	sstream.str(""); sstream.clear();
-	// N
+	// problems_in_wu
 	getline( master_config_file, input_str );
 	config_sstream << input_str << endl;
 	sstream << input_str;
-	sstream >> str1 >> str2 >> config_p.N;
-	sstream.str(""); sstream.clear();
-	// rows_count
-	getline( master_config_file, input_str );
-	config_sstream << input_str << endl;
-	sstream << input_str;
-	sstream >> str1 >> str2 >> config_p.rows_count;
-	sstream.str(""); sstream.clear();
-	// K
-	getline( master_config_file, input_str );
-	config_sstream << input_str << endl;
-	sstream << input_str;
-	sstream >> str1 >> str2 >> config_p.K;
+	sstream >> str1 >> str2 >> config_p.problems_in_wu;
 	sstream.str(""); sstream.clear();
 	// unsent_needed_wus
 	getline( master_config_file, input_str );
@@ -232,52 +233,24 @@ void ParseConfigFile( config_params &config_p, string &cnf_head, stringstream &c
 	sstream << input_str;
 	sstream >> str1 >> str2 >> config_p.created_wus;
 	sstream.str(""); sstream.clear();
-	// skip_values
-	getline( master_config_file, input_str );
-	sstream << input_str;
-	sstream >> str1 >> str2 >> config_p.skip_values;
-	sstream.str(""); sstream.clear();
-	// known_values
-	getline( master_config_file, input_str );
-	sstream << input_str;
-	int int_val;
-	vector<int> vec_int;
-	if ( input_str.find("known_values") != string::npos ) {
-		cout << "known_values_vec" << endl;
-		while( getline( master_config_file, input_str ) ) {
-			sstream.str(""); sstream.clear();
-			sstream << input_str;
-			while ( sstream >> int_val )
-				vec_int.push_back( int_val );
-			for ( unsigned i=0; i < vec_int.size(); ++i )
-				cout << vec_int[i] << " ";
-			cout << endl;
-			config_p.known_values_vec.push_back( vec_int );
-			vec_int.clear();
-			sstream.str(""); sstream.clear();
-		}
-	}
 	
-	sstream.str(""); sstream.clear();
-
 	// make cnf_head
 	sstream << "p cnf " << config_p.cnf_variables << " " << config_p.cnf_clauses;
 	cnf_head = sstream.str();
 	sstream.str(""); sstream.clear();
-
-	cout << "cnf_name "          << config_p.cnf_name << endl;
+	
+	cout << "problem_type "     << config_p.problem_type << endl;
+	cout << "settings_file "    << config_p.settings_file << endl;
+	cout << "data_file "        << config_p.data_file << endl;
 	cout << "cnf_variables "     << config_p.cnf_variables << endl;
 	cout << "cnf_clauses "       << config_p.cnf_clauses << endl;
-	cout << "N "                 << config_p.N << endl;
-	cout << "rows_count "        << config_p.rows_count << endl;
-	cout << "K "                 << config_p.K << endl;
+	cout << "problems_in_wu "    << config_p.problems_in_wu << endl;
 	cout << "unsent_needed_wus " << config_p.unsent_needed_wus << endl;
 	cout << "total_wus "         << config_p.total_wus << endl;
 	cout << "created_wus "       << config_p.created_wus << endl;
-	cout << "skip_values "       << config_p.skip_values << endl;
 	cout << "*** cnf_head "      << cnf_head << endl;
 	cout << endl;
-
+	
 	master_config_file.close();
 }
 
@@ -290,34 +263,20 @@ bool do_work( vector<int> &wu_id_vec )
 	processed_wus     = 0;
 	all_processed_wus = 0;
 	int unsent_count = 0;
-	config_params config_p;
+	config_params_crypto config_p;
 	stringstream config_sstream;
 	string cnf_head;
 	int wus_for_creation_count = 0;
 	
 	ParseConfigFile( config_p, cnf_head, config_sstream );
-	latin_square ls;
-	ls.N = config_p.N;
-	ls.K = config_p.K;
 	ls.rows_count = config_p.rows_count;
 	ls.skip_values = config_p.skip_values;
-	if ( config_p.cnf_name.find( "diag" ) != string::npos )
-		ls.problem_type = "diag";
-	if ( config_p.cnf_name.find( "inc0" ) != string::npos )
-		ls.problem_type = "inc0";
-	else if ( config_p.cnf_name.find( "inc70" ) != string::npos )
-		ls.problem_type = "inc70";
-	cout << "ls.N "            << ls.N << endl;
-	cout << "ls.rows_count "   << ls.rows_count << endl;
-	cout << "ls.K "            << ls.K << endl;
-	cout << "ls.skip_values "  << ls.skip_values << endl;
-	cout << "ls.problem_type " << ls.problem_type << endl;
 	bool IsLastGenerating = false;
-
-	if ( IsTasksFile ) // get PROBLEMS_IN_WU assumptions for every task
+	
+	/*if ( IsTasksFile ) // get problems_in_wu assumptions for every task
 		create_wus( ls, config_sstream, config_p, cnf_head, wus_for_creation_count, wu_id_vec, IsLastGenerating );
 	else
-	{
+	{*/
 		// create wus - supply needed level of unsent wus
 		wus_for_creation_count = 0;
 		int old_wus_for_creation_count = 0;
@@ -334,13 +293,13 @@ bool do_work( vector<int> &wu_id_vec )
 
 			GetCountOfUnsentWUs( unsent_count );
 			cout << "unsent_count " << unsent_count << endl;
-		
+			
 			if ( unsent_count < 0 ) {
-				cout << "SQL error. Skipping creating wus" << endl;
+				cout << "SQL error. unsent_count < 0. Waiting 60 seconds and try again" << endl;
 				sleep( 60 );
 				continue; // try to execute SQL again
 			}
-
+			
 			wus_for_creation_count = config_p.unsent_needed_wus - unsent_count;
 
 			if ( wus_for_creation_count + config_p.created_wus >= config_p.total_wus ) {
@@ -354,7 +313,7 @@ bool do_work( vector<int> &wu_id_vec )
 			if ( ( wus_for_creation_count >= MIN_WUS_FOR_CREATION ) || ( IsLastGenerating ) ) {
 				// ls can be used many times - each launch vectore will be resized and filled again
 				// ls.skip_valus is updated too
-				create_wus( ls, config_sstream, config_p, cnf_head, wus_for_creation_count, wu_id_vec, IsLastGenerating );
+				create_wus( config_sstream, config_p, cnf_head, wus_for_creation_count, wu_id_vec, IsLastGenerating );
 			}
 			else {
 				if ( old_wus_for_creation_count != wus_for_creation_count )
@@ -365,7 +324,7 @@ bool do_work( vector<int> &wu_id_vec )
 			if ( !IsLastGenerating )
 				sleep( 7200 ); // wait 2 hours
 		}
-	}
+	//}
 
 	cout << "wus_for_creation_count " << wus_for_creation_count << endl;
 	DC_log( LOG_NOTICE, "\n Work finished" );
@@ -376,52 +335,18 @@ bool do_work( vector<int> &wu_id_vec )
 	return 0;
 }
 
-void create_wus( latin_square &ls, stringstream &config_sstream, config_params &config_p, 
-	             string cnf_head, int wus_for_creation_count, vector<int> &wu_id_vec, bool &IsLastGenerating )
+void create_wus( stringstream &config_sstream, config_params_crypto &config_p, string cnf_head, 
+				 int wus_for_creation_count, vector<int> &wu_id_vec, bool &IsLastGenerating )
 {
-	int index = 0;
 	DC_Workunit *wu;
 	ofstream output;
 	string wu_tag_str;
-	stringstream sstream;
-
+	stringstream sstream, wu_sstream;
+	
 	cout << "Start create_wus()" << endl;
 	cout << "cnf_head " << cnf_head << endl;
 	cout << "cnf_name " << config_p.cnf_name << endl;
-
-	if ( IsTasksFile ) {
-		cout << "IsTasksFile mode" << endl;
-		ls.max_values_len = wu_id_vec[ wu_id_vec.size()-1 ] * PROBLEMS_IN_WU;
-		wus_for_creation_count = wu_id_vec.size();
-		config_p.created_wus = 0;
-	}
-	else
-		ls.max_values_len = wus_for_creation_count * PROBLEMS_IN_WU;
-	
 	cout << "wus_for_creation_count " << wus_for_creation_count << endl;
-	cout << "max_values_len " << ls.max_values_len << endl;
-	ls.verbosity = 0;
-
-	if ( config_p.known_values_vec.size() > 0 )
-		ls.known_values_vec = config_p.known_values_vec;
-
-	ls.MakeLatinValues();
-	cout << "MakeLatinValue() done" << endl;
-
-	if ( IsTasksFile ) {
-		int assumption_index;
-		vector< vector<int> > tmp_positive_literals;
-		for ( int i = 0; i < wu_id_vec.size(); i++ )
-			for ( int j = 0; j < PROBLEMS_IN_WU; j++ ) {
-				assumption_index = (wu_id_vec[i]-1) * PROBLEMS_IN_WU + j;
-				tmp_positive_literals.push_back( ls.positive_literals[assumption_index] );
-			}
-		ls.positive_literals = tmp_positive_literals;
-		cout << "new positive_literals.size() " << ls.positive_literals.size() << endl;
-	}
-	
-	ofstream tmp_out;
-	ls.WriteCurrentState( tmp_out, 0 );
 	cout << "Got latin values" << endl;
 	vector<int> :: iterator vec_it;
 	int values_index = 0;
@@ -429,50 +354,74 @@ void create_wus( latin_square &ls, stringstream &config_sstream, config_params &
 	bool IsAddingWUneeded;
 	bool IsFastExit = false;
 	unsigned new_created_wus = 0;
-	for( index = config_p.created_wus; index < config_p.created_wus + wus_for_creation_count; index++ ) {
+	string str;
+	ifstream ofile;
+	
+	// count blocks of data in file
+	short int si;
+	unsigned long ul;
+	ifile.open( config_p.data_file.c_str(), ios_base :: in | ios_base :: binary );
+	if ( !ifile.is_open() ) {
+		cerr << "!ifile.is_open() " << config_p.data_file << endl;
+		exit(1);
+	}
+	ifile.read( (char*)&si, sizeof(si) ); // read header
+	int assumptions_count = 0;
+	while ( ifile.read( (char*)&ul, sizeof(ul) ) )
+		assumptions_count++;
+	ifile.close();
+	
+	int total_wu_data_count = ceil( double(assumptions_count) / double(config_p.problems_in_wu) );
+	cout << "assumptions_count " << assumptions_count << endl;
+	cout << "wu_data_count "     << total_wu_data_count << endl;
+	
+	for( int wu_index = config_p.created_wus; wu_index < config_p.created_wus + wus_for_creation_count; wu_index++ ) {
 		if ( IsFastExit )
 			break;
+		ifile.open( config_p.settings_file.c_str() ); // write common head to every wu
+		if ( !ifile.is_open() ) {
+			cerr << "!ifile.is_open() " << config_p.settings_file << endl;
+			exit(1);
+		}
+		while ( getline( ifile, str ) )
+			wu_sstream << str << endl;
+		ifile.close();
+		
 		IsAddingWUneeded = false; // if no values will be added then WU not needed
-		//sstream << "c " << config_p.cnf_name << endl; // TODO return it after changing client app
-		sstream << "c ls_10_3_inc70" << endl;
-		for ( int i = 0; i < PROBLEMS_IN_WU; i++ ) {
-			sstream << "c ";
-			if ( values_index >= ls.positive_literals.size() ) {
-				cout << "in create_wus() last ls.positive_literals was added to WU" << endl;
+		for ( int i = 0; i < config_p.problems_in_wu; i++ ) {
+			if ( values_index >= total_wu_data_count ) {
+				cout << "in create_wus() last data was added to WU" << endl;
 				cout << "values_index " << values_index << endl;
 				IsFastExit = true; // add last values to WU and exit
 				IsLastGenerating = true; // tell to high-level function about ending of generation
 				break;
 			}
-			for ( vec_it = ls.positive_literals[values_index].begin(); vec_it != ls.positive_literals[values_index].end() - 1; vec_it++ )
-				sstream << *vec_it << " ";
-			sstream << *vec_it << endl;
+			// add binary data to wu_sstream
+			
 			values_index++;
 			IsAddingWUneeded = true;
 		}
-
+		
 		if ( !IsAddingWUneeded ) {
 			cout << "IsAddingWUneeded true" << endl;
 			break; // don't create new WU
 		}
-		sstream << cnf_head;
+		
 		output.open( "wu-input.txt", ios_base :: out );
 		if ( !output.is_open() ) {
 			DC_log(LOG_ERR, "Failed to create wu-input.txt: %s",
 			strerror(errno));
 			exit(1);
 		}
-		output << sstream.rdbuf();
-		sstream.str( "" ); sstream.clear();
+		output << wu_sstream.rdbuf();
+		wu_sstream.str( "" ); wu_sstream.clear();
 		output.close( );
 		
-		sstream << config_p.cnf_name;
-		if ( IsTasksFile ) // mark name with "additional" label
-			sstream << "-add";
-		sstream << "--" << index + 1; // save info about CNF name
+		sstream << config_p.problem_type;
+		sstream << "--" << wu_index + 1; // save info about CNF name
 		wu_tag_str = sstream.str();
 		sstream.str( "" ); sstream.clear();
-		wu = DC_createWU( "pdsat", NULL, 0, wu_tag_str.c_str() );
+		wu = DC_createWU( "pdsat_crypto", NULL, 0, wu_tag_str.c_str() );
 		if ( !wu ) {
 			DC_log( LOG_ERR, "Work unit creation has failed" );
 			exit(1);
@@ -496,9 +445,6 @@ void create_wus( latin_square &ls, stringstream &config_sstream, config_params &
 	
 	cout << "new_created_wus " << new_created_wus << endl;
 	config_p.created_wus += new_created_wus;
-	// update ls value
-	config_p.skip_values = ls.skip_values = ls.values_checked;
-	// update config file
 	
 	if ( !IsTasksFile ) { // don't update if additional wus from file ewre created
 		ofstream master_config_file;
@@ -506,77 +452,11 @@ void create_wus( latin_square &ls, stringstream &config_sstream, config_params &
 		master_config_file.open( master_config_file_name_str.c_str() );
 		master_config_file << config_sstream.str();
 		master_config_file << "created_wus = " << config_p.created_wus << endl; // update master config file
-		master_config_file << "skip_values = " << config_p.skip_values << endl; 
-		if ( config_p.known_values_vec.size() > 0 ) {
-			master_config_file << "known_values" << endl;
-			for ( unsigned i=0; i < config_p.known_values_vec.size(); ++i ) {
-				for ( unsigned j=0; j < config_p.known_values_vec[i].size(); ++j ) {
-					master_config_file << config_p.known_values_vec[i][j];
-					if ( j != config_p.known_values_vec[i].size() - 1 )
-						master_config_file << " ";
-				}
-				if ( i != config_p.known_values_vec.size() - 1 )
-					master_config_file << endl;
-			}
-		}
 		master_config_file.close();
 	}
 
 	cout << "created_wus " << config_p.created_wus << endl;
-	cout << "skip_values " << config_p.skip_values << endl;
 	cout << "---***---" << endl;
-}
-
-// Concatenate all results in their original order to form the final output
-// Try to find SAT in results
-bool find_sat( int cnf_index )
-{
-	bool flag = false;
-	ofstream output_file;
-	stringstream final_sstream;
-	stringstream sstream;
-	string final_output_name; 
-	
-	sstream << "final_output" << cnf_index << ".txt";
-	final_output_name = sstream.str( );
-	sstream.str( "" );
-	sstream.clear();
-	output_file.open( final_output_name.c_str() );
-	if ( !output_file.is_open() ) {
-		DC_log(LOG_ERR, "Cannot open final_output.txt for writing: %s",
-			strerror(errno));
-		exit(1);
-	}
-
-	string result_filename, input_str;
-	ifstream result_file;
-	/*for( int i = 1; i < created_wus + 1; i++ ) { // try all files
-		sstream << "result_" << i << ".txt";
-		result_filename = sstream.str();
-		sstream.str( "" );
-		sstream.clear();
-		result_file.open( result_filename.c_str(), ios_base :: in );
-		
-		if ( result_file.is_open( ) ) {     
-			//DC_log(LOG_NOTICE, "The result for WU %d exists ", i);
-			getline( result_file, input_str );
-			final_sstream << input_str;
-			final_sstream << " result_" << i << endl;
-			int sat_pos = input_str.find( "SAT" );
-			if ( !sat_pos ) { // if SAT then
-				flag = true;
-				while ( getline( result_file, input_str ) )
-					final_sstream << input_str << endl;
-			}
-			result_file.close();
-			result_file.clear();
-		}
-	}*/
-    
-	output_file << final_sstream.rdbuf();
-	output_file.close();
-	
-	return flag;
 }
 
 void add_result_to_file( string output_filename, char *tag, char *id )
@@ -706,4 +586,56 @@ bool ProcessQuery( MYSQL *conn, string str, vector< vector<stringstream *> > &re
 	mysql_free_result(res);
 
 	return true;
+}
+
+// Concatenate all results in their original order to form the final output
+// Try to find SAT in results
+bool find_sat( int cnf_index )
+{
+	bool flag = false;
+	ofstream output_file;
+	stringstream final_sstream;
+	stringstream sstream;
+	string final_output_name; 
+	
+	sstream << "final_output" << cnf_index << ".txt";
+	final_output_name = sstream.str( );
+	sstream.str( "" );
+	sstream.clear();
+	output_file.open( final_output_name.c_str() );
+	if ( !output_file.is_open() ) {
+		DC_log(LOG_ERR, "Cannot open final_output.txt for writing: %s",
+			strerror(errno));
+		exit(1);
+	}
+
+	string result_filename, input_str;
+	ifstream result_file;
+	/*for( int i = 1; i < created_wus + 1; i++ ) { // try all files
+		sstream << "result_" << i << ".txt";
+		result_filename = sstream.str();
+		sstream.str( "" );
+		sstream.clear();
+		result_file.open( result_filename.c_str(), ios_base :: in );
+		
+		if ( result_file.is_open( ) ) {     
+			//DC_log(LOG_NOTICE, "The result for WU %d exists ", i);
+			getline( result_file, input_str );
+			final_sstream << input_str;
+			final_sstream << " result_" << i << endl;
+			int sat_pos = input_str.find( "SAT" );
+			if ( !sat_pos ) { // if SAT then
+				flag = true;
+				while ( getline( result_file, input_str ) )
+					final_sstream << input_str << endl;
+			}
+			result_file.close();
+			result_file.clear();
+		}
+	}*/
+    
+	output_file << final_sstream.rdbuf();
+	output_file.close();
+	
+	return flag;
 }
