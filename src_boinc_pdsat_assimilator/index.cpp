508cbde38d9d569ef9d061f6585512e98ea08b93
Oleg Zaikin <zaikin.icc@gmail.com>
2015-06-12 20:01:46 +0800

    *

--- src_boinc_pdsat_assimilator/index.cpp
+++ src_boinc_pdsat_assimilator/index.cpp
@@ -4,7 +4,7 @@
 #include <string>
 #include <sstream>
 #include "Mols.h"
-//#include <omp.h>
+#include <cmath>
 
 using namespace std;
 
@@ -12,19 +12,29 @@ int main()
 {
 	ifstream ifile("sat_output");
 	string str;
-	getline(ifile,str);
-	ifile.close();
-	cout << str << endl;
-	stringstream sstream;
-	sstream << str;
-	string sat_assign_str = "";
-	while ((sat_assign_str.length() < 100) && (!sstream.eof()) )
-		sstream >> sat_assign_str;
+	int n = 10, r = 2;
+	unsigned pair_count = 0;
+	while (getline(ifile, str)) {
+		std::cout << std::endl << "pair_count " << pair_count << std::endl;
+		/*if ((pair_count == 1) || (pair_count == 2) || (pair_count == 3)) {
+			pair_count++;
+			continue;
+		}*/
+		cout << str << endl;
+		stringstream sstream;
+		sstream << str;
+		string sat_assign_str = "";
+		while ((sat_assign_str.length() < 100) && (!sstream.eof()))
+			sstream >> sat_assign_str;
 
-	int n = 10;
-	int r = 2;
-	MOLS mols(sat_assign_str, n, r, false);
-	mols.print("squares");
+		if (sat_assign_str.size() == r*pow(n, 3)) {
+			std::cout << "new pair" << std::endl;
+			MOLS mols(sat_assign_str, 10, 2, false);
+			//mols.printToCout();
+		}
+		pair_count++;
+	}
+	ifile.close();
 
 	return 0;
 }
