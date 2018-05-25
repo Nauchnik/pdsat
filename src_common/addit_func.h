56b8d2ef549c4bb873a43cc1a9447f69eff61df4
Oleg Zaikin <zaikin.icc@gmail.com>
2018-05-25 19:10:59 +0800

    + strtoint() and similar functions

--- src_common/addit_func.h
+++ src_common/addit_func.h
@@ -17,6 +17,8 @@
 #include <dirent.h>
 #endif
 
+using namespace std;
+
 namespace Addit_func {
 
 double cpu_time(void);
@@ -25,11 +27,11 @@ double cpu_time(void);
 extern bool isNumber( char num );
 extern bool isNumberOrMinus( char num );
 extern void cpuTimeInHours( double full_seconds, int &real_hours, int &real_minutes, int &real_seconds );
-extern void MakeCombinations(long long n, long long k, std::vector< std::vector<long long> > &combinations, bool isOnlySizeCalc = false);
-extern void MakePermutations(long long n, long long k, std::vector< std::vector<long long> > &permutations);
+extern void MakeCombinations(long long n, long long k, vector< vector<long long> > &combinations, bool isOnlySizeCalc = false);
+extern void MakePermutations(long long n, long long k, vector< vector<long long> > &permutations);
 extern long long ConseqMultip(long long low_bound, long long high_bound);
 extern int BitCount( unsigned u );
-extern bool getdir( std::string dir, std::vector<std::string> &files );
+extern bool getdir( string dir, vector<string> &files );
 //extern boost::dynamic_bitset<> IntVecToBitset( unsigned bitset_len, vector<int> &vec_int );
 //extern vector<int> BitsetToIntVec( boost::dynamic_bitset<> &bs );
 extern void shl64( unsigned long long int &val_for_left_shift, unsigned int bit_count );
@@ -37,16 +39,16 @@ extern unsigned long long BitsetToUllong( boost::dynamic_bitset<> cur_bitset );
 extern void UllongToBitset( unsigned long long ull, boost::dynamic_bitset<> &bs );
 extern unsigned uint_rand( boost::random::mt19937 &gen );
 extern bool bool_rand( boost::random::mt19937 &gen );
-extern std::string exec( std::string cmd_str );
+extern string exec( string cmd_str );
 
 template< typename T > 
-bool next_cartesian( std::vector<T> &vii, std::vector<int> &index_arr, T &cur_vi )
+bool next_cartesian( vector<T> &vii, vector<int> &index_arr, T &cur_vi )
 {
 	if( index_arr.size() == 0 ) { // init
 		index_arr.resize( vii.size() );
 		//for( auto &x : index_arr )
 		//	x = 0;
-		for( std::vector<int> :: iterator it = index_arr.begin(); it != index_arr.end(); ++it )
+		for( vector<int> :: iterator it = index_arr.begin(); it != index_arr.end(); ++it )
 			*it = 0;
 	}
 	if( index_arr[0] == -1 )
@@ -79,6 +81,51 @@ bool next_cartesian( std::vector<T> &vii, std::vector<int> &index_arr, T &cur_vi
 	}
 	
 	return true;
+}
+
+inline string doubletostr(const double x)
+{
+	string str;
+	stringstream sstream;
+	sstream << x;
+	sstream >> str;
+	return str;
+}
+
+inline double strtodouble(const string str)
+{
+	double a;
+	stringstream sstream;
+	sstream << str;
+	sstream >> a;
+	return a;
+}
+
+inline unsigned strtouint(const string str)
+{
+	unsigned a;
+	stringstream sstream;
+	sstream << str;
+	sstream >> a;
+	return a;
+}
+
+inline string uinttostr(unsigned x)
+{
+	stringstream sstream;
+	sstream << x;
+	string str;
+	sstream >> str;
+	return str;
+}
+
+inline int strtoint(const string str)
+{
+	int a;
+	stringstream sstream;
+	sstream << str;
+	sstream >> a;
+	return a;
 }
 
 }
